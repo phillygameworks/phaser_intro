41ab37b16110b4bec41c94c660297f0041fc56a9
Chris Gomez <cagomez@cellbycell.com>
2015-03-21 06:58:02 -0400

    Add assets, show sprite

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-﻿var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+﻿/// <reference path="phaser.min.js" />
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
-}
+    game.load.image('star', 'assets/star.png');
+ }
 
 function create() {
+    game.add.sprite(0, 0, 'star');
 }
 
 function update() {
